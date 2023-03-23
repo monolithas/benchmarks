74a768e96b90cc19c6208c2eed784d47cab99293
Michael House <mjhouse@protonmail.com>
2023-03-22 22:54:18 -0500

    Added display script

---
+++ benchmarks/display.py
@@ -0,0 +1,17 @@
+import toml
+import argparse
+
+from pathlib import Path
+
+from benchmarks.result import SummaryResult, SeriesResult
+from benchmarks.utilities import setup_logger
+
+def run():
+
+    # convert all 'output/results/*' to SeriesResult objects
+
+    # convert the 'output/summary.json' file to a SummaryResult object
+
+    # build charts or graphes to compare the languages
+
+    print("Visualize data somehow")
